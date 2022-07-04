78d39fada930bad9396aef747ce2181eeee6072c
Eduard <eduard@localhost.localdomain>
2022-07-04 00:38:59 -0400

    log

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model{
     protected $table="usuario";
     public $timestamps=false;
-    protected $fillable = ['ID','Usuario','Pwd','Persona','Rango','Perfil','FechaCreado','FechaActualizado','FechaEliminado'];
+    protected $fillable = ['ID','Usuario','Pwd','Perfil','FechaCreado','FechaActualizado','FechaEliminado'];
 }
 ?>
--- graphql/query.php
+++ graphql/query.php
@@ -1,7 +1,6 @@
 <?php
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
-use App\Models\Usuario;
 $rootQuery=new ObjectType([
     'name'=>'Query',
     'fields'=>[
