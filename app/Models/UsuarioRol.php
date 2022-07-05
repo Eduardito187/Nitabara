6b334c2577096f8176661fd7dcde6449ed79b643
Eduard <eduard@localhost.localdomain>
2022-07-05 01:24:40 -0400

    ok

--- app/Models/UsuarioRol.php
+++ app/Models/UsuarioRol.php
@@ -1,9 +1,17 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Rol;
+use App\Models\Usuario;
 class UsuarioRol extends Model{
     protected $table="usuario_rol";
     public $timestamps=false;
     protected $fillable = ['ID','Rol','Usuario','FechaCreado','FechaActualizado','FechaEliminado'];
+    public function rol(){
+        return $this->hasOne(Rol::class,'ID','Usuario');
+    }
+    public function usuario(){
+        return $this->hasOne(Usuario::class,'ID','Usuario');
+    }
 }
 ?>
