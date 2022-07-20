b7704341013d6e18e1d3e76ecd9a236870ef10a2
Eduard <eduard@localhost.localdomain>
2022-07-20 16:23:30 -0400

    ok

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -1,9 +1,25 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Usuario;
+use App\Models\TipoDocumento;
+use App\Models\Ciudad;
+use App\Models\Direccion;
 class Persona extends Model{
     protected $table="persona";
     public $timestamps=false;
     protected $fillable = ['ID','Nombre','Paterno','Materno','CI','Correo','Telefono','Nacimiento','TipoDocumento','Direccion','Ciudad','Usuario','FechaCreado','FechaActualizado','FechaEliminado'];
+    public function usuario_r(){
+        return $this->hasOne(Usuario::class,'ID','Usuario');
+    }
+    public function ciudad_r(){
+        return $this->hasOne(Ciudad::class,'ID','Ciudad');
+    }
+    public function direccion_r(){
+        return $this->hasOne(Direccion::class,'ID','Direccion');
+    }
+    public function tipo_documento_r(){
+        return $this->hasOne(TipoDocumento::class,'ID','TipoDocumento');
+    }
 }
 ?>
