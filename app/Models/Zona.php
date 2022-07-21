cfe50d81f4762545f99646e612690a4a1f625df4
Eduard <eduard@localhost.localdomain>
2022-07-21 01:07:06 -0400

    cambios ok

--- app/Models/Zona.php
+++ app/Models/Zona.php
@@ -1,8 +1,8 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class PersonaConsulta extends Model{
-    protected $table="persona_consulta";
+class Zona extends Model{
+    protected $table="zona";
     public $timestamps=false;
     protected $fillable = ['ID','Nombre','FechaCreado','FechaActualizado','FechaEliminado'];
 }
