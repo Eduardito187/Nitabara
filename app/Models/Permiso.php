7c54373757384a93380b03bc82ed733f570b6c88
Eduard <eduard@localhost.localdomain>
2022-07-22 17:58:13 -0400

    Roles

--- app/Models/Permiso.php
+++ app/Models/Permiso.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\RolPermiso;
 class Permiso extends Model{
     protected $table="permiso";
     public $timestamps=false;
     protected $fillable = ['ID','Nombre','Codigo','FechaCreado','FechaActualizado','FechaEliminado'];
+    public function rol_permiso_r(){
+        return $this->hasMany(RolPermiso::class,'Permiso','ID');
+    }
 }
 ?>
--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\RolPermiso;
 class Rol extends Model{
     protected $table="rol";
     public $timestamps=false;
     protected $fillable = ['ID','Rol','FechaCreado','FechaActualizado','FechaEliminado'];
+    public function rol_permiso_r(){
+        return $this->hasMany(RolPermiso::class,'Rol','ID');
+    }
 }
 ?>
