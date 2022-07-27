df66f31327de6660edac9caa5cc0bcbb89f2d180
Eduard <eduard@localhost.localdomain>
2022-07-27 03:12:44 -0400

    roles

--- graphql/mutations.php
+++ graphql/mutations.php
@@ -3,9 +3,11 @@ use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 
 require('mutations/Usuario.php');
+require('mutations/Rol.php');
 
 $mutations=array();
 $mutations+=$Usuario;
+$mutations+=$Rol;
 $rootMutation=new ObjectType([
     'name'=>'Mutation',
     'fields' => $mutations
