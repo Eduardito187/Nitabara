86b3f05932ff7f2cfa2e599f47874141b896d41c
Eduard <eduard@localhost.localdomain>
2022-08-28 21:47:59 -0400

    change

--- app/Models/Consulta.php
+++ app/Models/Consulta.php
@@ -3,15 +3,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Medico;
 use App\Models\ConsultaPago;
+use App\Models\Persona;
 class Consulta extends Model{
     protected $table="consulta";
     public $timestamps=false;
-    protected $fillable = ['ID','Medico','Descripcion','Hora','FechaCreado','FechaActualizado','FechaEliminado'];
+    protected $fillable = ['ID','Persona','Medico','Descripcion','Hora','FechaCreado','FechaActualizado','FechaEliminado'];
     public function medico_r(){
         return $this->hasOne(Medico::class,'ID','Medico');
     }
     public function consulta_pago_r(){
         return $this->hasOne(ConsultaPago::class,'Consulta','ID');
+    }
+    public function persona_r(){
+        return $this->hasOne(Persona::class,'ID','Persona');
     }
 }
 ?>
