08dc2eb51b6923e8d4c7f8f291257c6666494100
Eduard <eduard@localhost.localdomain>
2022-09-02 00:16:43 -0400

    change

--- graphql/query.php
+++ graphql/query.php
@@ -237,7 +237,8 @@ $rootQuery=new ObjectType([
                 $Permisos = Permiso::where("Codigo",$args["Codigo"])->with(['rol_permiso_r'])->get();
                 foreach ($Permisos as $permiso) {
                     if ($permiso->rol_permiso_r != null) {
-                        $roles_user = UsuarioRol::where("Usuario",$args["ID"])->where("Rol",$permiso->rol_permiso_r->Rol)->get();
+                        print_r($permiso->rol_permiso_r->Rol);
+                        $roles_user = UsuarioRol::where("Usuario",$args["ID"])->where("Rol",$permiso->rol_permiso_r->Rol)->first();
                         if ($roles_user != null) {
                             return true;
                         }
